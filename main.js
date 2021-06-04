2892a5a6dee5ca278fa6de5ce4d19f4fcd16ed8f
Hemanth1599 <78473917+Hemanth1599@users.noreply.github.com>
2021-06-04 16:12:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot(){
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/TdYplXUfZ/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/SrcDPYsOX/model.json', modelLoaded);
 
 function modelLoaded(){
     console.log('modelLoaded');
